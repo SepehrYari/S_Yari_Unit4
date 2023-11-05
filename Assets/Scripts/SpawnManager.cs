71c4573ed13a557ea1506b7b38aa4e42b8a5d7e2
SepehrYari <91702741+SepehrYari@users.noreply.github.com>
2023-11-04 21:26:32 -0700

    S_Yari_Unit4_LessonD
    Added Wave rounds with looping code

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-
+    public GameObject powerUpPrefab;
     public GameObject enemyPrefab;
     private float spawnRange = 8.5f;
+    private int enemyCount;
+
+    private int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
         //Vector3 GenPos = GenerateSpawnPosition();
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnWave(waveNumber);
+        
+
     }
+
+    private void Update()
+    {
+        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        //Debug.Log(FindObjectsOfType<Enemy>().Length);
+        Debug.Log(enemyCount);
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnWave(waveNumber);
+        }
+        
+    }
+
+    void SpawnWave(int enemyNum)
+    {
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+
+        for (int i = 0; i < enemyNum; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
 
     Vector3 GenerateSpawnPosition()
     {
